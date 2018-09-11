61533dacc10260912cc973c31286553d50298b7b
ztr1szh <Tracy.ZHU@cn.bosch.com>
2018-09-11 14:45:25 +0800

    commint on 2018/9/11

--- app/src/main/java/com/example/tracy/coolweather/util/Utility.java
+++ app/src/main/java/com/example/tracy/coolweather/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.example.tracy.coolweather.db.City;
 import com.example.tracy.coolweather.db.County;
 import com.example.tracy.coolweather.db.Province;
+import com.example.tracy.coolweather.gson.Weather;
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 import org.json.JSONArray;
@@ -88,4 +90,22 @@ public class Utility {
         }
         return false;
     }
+
+
+    public static Weather handleWeatherResponse(String response) {
+
+        try {
+
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
 }
